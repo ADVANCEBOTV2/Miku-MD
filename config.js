16a8300768779ccf7b5efd75e3d545b278bf5434
ADVANCEBOTV2 <115334030+ADVANCEBOTV2@users.noreply.github.com>
2022-10-08 09:45:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.Owner = ['919755062790']
+global.OwnerNumber = ['919755062790']
+global.ownertag = ['919755062790']
+global.BotName = "furyv2"
+global.packname = "nikki bot"
+global.author = "By: nikhil"
+global.OwnerName = "nikhil"
+global.BotSourceCode = "https://github.com"
+global.SupportGroupLink = "https"
 global.sessionName = "session"
 
 // Prefix //
@@ -23,7 +23,7 @@ global.sessionName = "session"
 global.prefa = ['-'] 
 
 
-global.location = "West Bengal, India"
+global.location = "madhya pradesh, India"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
